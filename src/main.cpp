60b00f833631262c56577329f5fd547a3d983bd1
Manuel Altalef <maltalef@auror.ar>
2021-04-24 16:41:22 -0300

    fix(main): agregado de \\n al printf

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
     for (u32 i = 0; i < LENGTH(blocks); i++)
     {
         Block block = blocks[i];
-        printf("block %i: command => '%s' interval => '%i' signal => '%i'", i, block.get_cmd(), block.get_interval(), block.get_sig());
+        printf("block %i: command => '%s' interval => '%i' signal => '%u'\n", i, block.get_cmd(), block.get_interval(), block.get_sig());
     }
 
     return 0;
